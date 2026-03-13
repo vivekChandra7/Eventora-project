a94f0560f06cf67955e8b843768d285309491d35
vivekChandra7 <vivekchandra2462@gmail.com>
2026-03-13 23:55:34 +0530

    controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,10 +3,21 @@ const User = require('../models/User');
 // Register user
 exports.registerUser = async (req, res) => {
     const { name, email, password, role } = req.body;
+   
+    let userExists =await User.findOne({ email });
+    if(userExists){
+       return res.status(400).json({ message: 'User already exists' });
+    }
+     const salt = await bcrypt.genSalt(10);
+     const hashedPassword = await bcrypt.hash(password, salt);  
+     
 
-    
+     const otp = Math.floor(100000 + Math.random() * 900000).toString(); // Generate a 6-digit OTP
+     console.log('Generated OTP:', otp); // Log the OTP for testing purposes
+     
+
     try{
-        const user = new User({ name, email, password, role });
+        const user = new User({ name, email, password:hashedPassword, role });
         await user.save();
         res.status(201).json({ message: 'User registered successfully' });      
     }catch (error) {
