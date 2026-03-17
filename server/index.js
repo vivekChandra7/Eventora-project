fed323199b3fdaac28b7c5e9b220d8004f19fb62
vivekChandra7 <vivekchandra2462@gmail.com>
2026-03-17 23:52:00 +0530

    initialize express server with auth, events, and booking routes

--- server/index.js
+++ server/index.js
@@ -1,27 +1,28 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 dotenv.config();
-const cors = require('cors');
-const mongoose = require('mongoose');
-const authRoutes = require('./routes/auth');
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const eventRoutes = require("./routes/events.js");
+const bookingRoutes = require('./routes/booking.js');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 //routes
-app.use('/api/auth', authRoutes);
-
-
+app.use("/api/auth", authRoutes);
+app.use("/api/events", eventRoutes);
+app.use("/api/bookings", bookingRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  
-})
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGO_URI, {})
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-}); 
+});
