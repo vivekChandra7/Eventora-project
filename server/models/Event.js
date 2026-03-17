8a13bac43b45d6ac9b1606d59257960b0882bb36
vivekChandra7 <vivekchandra2462@gmail.com>
2026-03-17 23:52:50 +0530

    event model

---
+++ server/models/Event.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    totalSeats: {
+        type: Number,
+        required: true,
+    },
+    availableSeats: {
+        type: Number,
+        required: true,
+    }, 
+    ticketPrice: {
+        type: Number,
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Event', eventSchema);
