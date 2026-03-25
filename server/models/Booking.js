894a3b15a77ef41a4cc9495be12753763ad00da7
vivekChandra7 <vivekchandra2462@gmail.com>
2026-03-25 16:30:03 +0530

    booking model

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -1,29 +1,30 @@
-const mongoose = require('mongoose');
-const bookingSchema = new mongoose.Schema({
-    userId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-    }
-    ,eventId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Event',
-        required: true,
+const mongoose = require("mongoose");
+const bookingSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    eventId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+      required: true,
     },
     status: {
-        type: String,
-        enum: ['pending', 'booked', 'cancelled'],
-        default: 'pending',
+      type: String,
+      enum: ["confirmed", "cancelled", "pending"],
+      default: "pending",
     },
     paymentStatus: {
-        type: String,
-        enum: ['non_paid', 'paid', 'failed'],
-        default: 'non_paid',
+      type: String,
+      enum: ["paid", "not_paid"],
+      default: "not_paid",
     },
-    amount: {
-        type: Number,
-        required: true,
-    },
-}, { timestamps: true });   
+    amount: { type: Number, required: true },
+    bookedAt: { type: Date, default: Date.now },
+  },
+  { timestamps: true },
+);
 
-module.exports = mongoose.model('Booking', bookingSchema);
+module.exports = mongoose.model("Booking", bookingSchema);
